79e1e08f5c9a99335c54ad62117bfe10ec772602
Jiawei Li <1740459051@qq.com>
2020-08-26 12:27:39 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,12 +30,12 @@
 			xsmall:  [ null,      '480px'  ],
 		});
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+// 	// Play initial animations on page load.
+// 		$window.on('load', function() {
+// 			window.setTimeout(function() {
+// 				$body.removeClass('is-preload');
+// 			}, 100);
+// 		});
 
 	// Touch?
 		if (browser.mobile) {
@@ -112,5 +112,12 @@
 				});
 
 			});
+	
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
 
 })(jQuery);
